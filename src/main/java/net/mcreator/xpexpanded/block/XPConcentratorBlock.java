734600be90e189a991090738d5a1a7a18bf4821f
TrollfaceDev <jackc@GAMEPC>
2021-05-04 19:48:55 +0100

    Simpilification of code.

--- src/main/java/net/mcreator/xpexpanded/block/XPConcentratorBlock.java
+++ src/main/java/net/mcreator/xpexpanded/block/XPConcentratorBlock.java
@@ -2,32 +2,39 @@
 package net.mcreator.xpexpanded.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.common.ToolType;
 
 import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Direction;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
-import net.mcreator.xpexpanded.procedures.OpenConcentrationGUIProcedure;
+import net.mcreator.xpexpanded.gui.XPConcentratorGUIGui;
 import net.mcreator.xpexpanded.XpExpandedModElements;
 
-import java.util.Map;
 import java.util.List;
-import java.util.HashMap;
 import java.util.Collections;
+
+import io.netty.buffer.Unpooled;
 
 @XpExpandedModElements.ModElement.Tag
 public class XPConcentratorBlock extends XpExpandedModElements.ModElement {
@@ -65,15 +72,19 @@ public class XPConcentratorBlock extends XpExpandedModElements.ModElement {
 			int x = pos.getX();
 			int y = pos.getY();
 			int z = pos.getZ();
-			Direction direction = hit.getFace();
-			{
-				Map<String, Object> $_dependencies = new HashMap<>();
-				$_dependencies.put("entity", entity);
-				$_dependencies.put("x", x);
-				$_dependencies.put("y", y);
-				$_dependencies.put("z", z);
-				$_dependencies.put("world", world);
-				OpenConcentrationGUIProcedure.executeProcedure($_dependencies);
+			if (entity instanceof ServerPlayerEntity) {
+				NetworkHooks.openGui((ServerPlayerEntity) entity, new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new StringTextComponent("XP Concentrator");
+					}
+
+					@Override
+					public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+						return new XPConcentratorGUIGui.GuiContainerMod(id, inventory,
+								new PacketBuffer(Unpooled.buffer()).writeBlockPos(new BlockPos(x, y, z)));
+					}
+				}, new BlockPos(x, y, z));
 			}
 			return ActionResultType.SUCCESS;
 		}
